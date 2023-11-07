c8c69656df9137760f6c78a3bb3b2a488b73f205
rspakal <gibsrom@gmail.com>
2023-11-07 12:55:57 +0100

    Add project files.

---
+++ SF15/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+            Console.ReadKey();
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var students = from c in classes from s in c.Students select s;
+            return students.ToArray();
+        }
+
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
